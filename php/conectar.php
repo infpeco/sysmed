a42b3fab7780474a2b511539bf0b585f4dd5e3de
Infante <infpedro@gmail.com>
2019-12-01 22:54:26 -0400

    quitando utilitario y probando

--- php/conectar.php
+++ php/conectar.php
@@ -1,7 +1,7 @@
 <?php
 $server = "localhost";
 $usuario = "root";
-$password = "nomejoda";
+$password = "/1LwU4caFTN5mSkfzr2V";
 $db = "sysmed";
 $result = mysqli_connect($server, $usuario, $password, $db);
 if (mysqli_connect_errno()) {  
